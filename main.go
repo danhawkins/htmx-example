13eb31a9e8512d059aa7a3367ab8fc2fd8c1a358
Danny Hawkins <danny@quiqup.com>
2023-11-09 23:41:59 +0400

    Adds HTMX example

--- main.go
+++ main.go
@@ -12,21 +12,36 @@ type Film struct {
 }
 
 func main() {
+	data := map[string][]Film{
+		"Films": {
+			{"The Shawshank Redemption", "Frank Darabont"},
+			{"The Godfather", "Francis Ford Coppola"},
+			{"The Godfather: Part II", "Francis Ford Coppola"},
+			{"The Dark Knight", "Christopher Nolan"}},
+	}
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.htm"))
-
-		data := map[string][]Film{
-			"Films": {
-				{"The Shawshank Redemption", "Frank Darabont"},
-				{"The Godfather", "Francis Ford Coppola"},
-				{"The Godfather: Part II", "Francis Ford Coppola"},
-				{"The Dark Knight", "Christopher Nolan"}},
-		}
-
 		tmpl.Execute(w, data)
 	}
 
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+
+		newFilm := Film{title, director}
+		data["Films"] = append(data["Films"], newFilm)
+
+		newLi := `
+		<li class="list-group-item bg-primary text-white">
+			{{ .Title }} - {{ .Director }}
+		</li>
+		`
+		tmpl, _ := template.New("t").Parse(newLi)
+		tmpl.Execute(w, newFilm)
+	}
+
+	http.HandleFunc("/add-film/", h2)
 	http.HandleFunc("/", h1)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
